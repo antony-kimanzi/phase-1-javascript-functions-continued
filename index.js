f97293051f45c4fa94201b13a4849f7a7f82251e
Antony Kimanzi <antonykimanzi29@gmail.com>
2024-09-26 22:35:24 +0300

    edit index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun();
+saturdayFun("play basketball");
+
+let mondayWork = function (task = "go to the office"){
+    return `This Monday, I will ${task}.`
+}
+
+mondayWork();
+mondayWork("work from home");
+
+let wrapAdjective = function (par1 = "*") {
+    return function (par2 = "special") {
+        return `You are ${par1}${par2}${par1}!`;
+    }
+}
+
+let result = wrapAdjective();
+let result2 = wrapAdjective("||")("a dedicated programmer");
